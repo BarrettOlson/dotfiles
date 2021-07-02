b885f09f5999d3814e645664cb0eecf422b404e9
Barrett Olson <barrett@fb.com>
2021-07-02 12:29:00 -0400

    update ingore and mac_setup

--- mac_setup.sh
+++ mac_setup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install zsh tmux coreutils vim gnu-sed openssl fzf tree rsync
-brew cask install alacritty
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install zsh tmux coreutils vim gnu-sed openssl fzf tree rsync jq et
 sudo bash -c 'echo "/usr/local/bin/zsh" >> /etc/shells'
 chsh -s /usr/local/bin/zsh
